b0b487cac7f8f9cf242ff53ff3bb5697b83e0abb
cmoser96 <cm898946@gmail.com>
2018-10-10 18:54:37 -0400

    working on inputconditioner test

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -1,3 +1,4 @@
+`include "inputconditioner.v"
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
@@ -10,20 +11,57 @@ module testConditioner();
     wire rising;
     wire falling;
     
+    reg dutpassed = 1;
+
     inputconditioner dut(.clk(clk),
-    			 .noisysignal(pin),
-			 .conditioned(conditioned),
-			 .positiveedge(rising),
-			 .negativeedge(falling))
+                 .noisysignal(pin),
+             .conditioned(conditioned),
+             .positiveedge(rising),
+             .negativeedge(falling));
 
 
     // Generate clock (50MHz)
+    initial pin=0;
     initial clk=0;
     always #10 clk=!clk;    // 50MHz Clock
-    
+
     initial begin
-    // Your Test Code
-    // Be sure to test each of the three conditioner functions:
-    // Synchronization, Debouncing, Edge Detection
-    
+    $display("Begin testing inputconditioner.v");
+
+    genvar i;
+    generate
+    for(i=0; i<7; i=i+1)
+        begin:genblock
+            pin=!pin; #3
+            $display("pin  | conditioned | rising | falling");
+            $display(" %b  | %b          | %b     |  %b", pin, conditioned, rising, falling);
+        end
+    endgenerate
+
+    pin=!pin #100
+    $display("pin  | conditioned | rising | falling");
+    $display(" %b  | %b          | %b     |  %b", pin, conditioned, rising, falling);
+
+
+    genvar i;
+    generate
+    for(i=0; i<7; i=i+1)
+        begin:genblock
+            pin=!pin; #3
+            $display("pin  | conditioned | rising | falling");
+            $display(" %b  | %b          | %b     |  %b", pin, conditioned, rising, falling);
+        end
+    endgenerate
+
+    pin=!pin; #100
+    $display("pin  | conditioned | rising | falling");
+    $display(" %b  | %b          | %b     |  %b", pin, conditioned, rising, falling);
+
+    if (dutpassed) begin
+      $display("Tests passed");
+    end else begin
+      $display("Tests completed");
+    end
+
+  end
 endmodule
